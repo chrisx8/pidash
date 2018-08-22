6afe5334d5cc1025da335ede03ee85eb3acf6d7a
chrisx8 <30990835+chrisx8@users.noreply.github.com>
2018-08-22 17:25:09 -0400

    add logout button and reverse proxy support

--- config.py
+++ config.py
@@ -1,6 +1,11 @@
 # put your HOSTNAME here if you want to use a customized hostname
 CUSTOM_HOSTNAME = ""
 
+# If the dashboard is behind a reverse proxy, enter the path here
+# Include a leading slash but no trailing slashes
+# LEAVE EMPTY if the path is '/' or not using a reverse proxy
+URL_PREFIX = ""
+
 # make a good secret key!
 SECRET_KEY = "SECRET_KEY"
 
